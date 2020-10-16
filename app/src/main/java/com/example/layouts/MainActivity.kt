96bfadcf178196f90d68b263a59dbb689f2822c9
Mohit Kumar <mohitk02082000@gmail.com>
2020-10-16 17:56:26 +0530

    -> working on add Payment layout

--- app/src/main/java/com/example/layouts/MainActivity.kt
+++ app/src/main/java/com/example/layouts/MainActivity.kt
@@ -4,8 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.example.layouts.manageTheRenters.MTRHomeActivity
-import com.example.layouts.manageTheRenters.TestActivity
+import com.example.layouts.manageTheRenters.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -21,6 +20,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private fun initListener() {
 
         mtrHomeBtn.setOnClickListener(this)
+        mtrAddPayment.setOnClickListener(this)
+        mtrAddRenter.setOnClickListener(this)
+        mtrProfile.setOnClickListener(this)
+        mtrShowBill.setOnClickListener(this)
+        mtrRenter.setOnClickListener(this)
         testActivityBtn.setOnClickListener(this)
     }
 
@@ -31,6 +35,36 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             mtrHomeBtn.id -> {
 
                 val intent = Intent(this@MainActivity, MTRHomeActivity::class.java)
+                startActivity(intent)
+            }
+
+            mtrAddPayment.id -> {
+
+                val intent = Intent(this@MainActivity, MTRAddPaymentActivity::class.java)
+                startActivity(intent)
+            }
+
+            mtrAddRenter.id -> {
+
+                val intent = Intent(this@MainActivity, MTRAddRenterActivity::class.java)
+                startActivity(intent)
+            }
+
+            mtrProfile.id -> {
+
+                val intent = Intent(this@MainActivity, MTRProfileActivity::class.java)
+                startActivity(intent)
+            }
+
+            mtrRenter.id -> {
+
+                val intent = Intent(this@MainActivity, MTR_RenterActivity::class.java)
+                startActivity(intent)
+            }
+
+            mtrShowBill.id -> {
+
+                val intent = Intent(this@MainActivity, MTRShowBillActivity::class.java)
                 startActivity(intent)
             }
 
